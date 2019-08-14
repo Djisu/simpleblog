7ed7d459a14aba71df18b3eedf65d3abc10ee688
Djisu <pfleischer2002@yahoo.co.uk>
2019-08-14 00:04:51 -0700

    Another commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,21 @@
 class PostsController < ApplicationController
     def index 
-
+        @posts = Post.all
+    end
+    def show
+        @post = Post.find(params[:id])
     end
     def new
-        
+       @post = Post.new
     end
     def create 
-        #render plain: params[:post].inspect
         @post = Post.new(post_params)
+
+        if (@post.save)
+            redirect_to @post
+        else
+            render "new"
+        end
     end
 
     private def post_params
